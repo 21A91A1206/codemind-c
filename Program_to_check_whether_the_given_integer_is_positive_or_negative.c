5964cf40303acba083f36469ccb6759d818a9177
21A91A1206 <98875470+21A91A1206@users.noreply.github.com>
2022-02-13 03:08:46 +0000

    #include<stdio.h> int main() {     float n;     scanf("%f",&n);     if(n<0)     {     printf("Negative Number");     }     else     {         printf("Positive Number");     }     return 0; }

---
+++ Program_to_check_whether_the_given_integer_is_positive_or_negative.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    float n;
+    scanf("%f",&n);
+    if(n<0)
+    {
+    printf("Negative Number");
+    }
+    else
+    {
+        printf("Positive Number");
+    }
+    return 0;
+}
